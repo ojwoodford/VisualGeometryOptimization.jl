9464120914161f7693ffeca44f7a3134ba135f7a
Oliver J. Woodford <o.j.woodford.98@cantab.net>
2023-11-02 14:06:16 +0000

    Files generated by PkgTemplates
    PkgTemplates version: 0.7.45

---
+++ src/VisualGeometryOptimization.jl
@@ -0,0 +1,5 @@
+module VisualGeometryOptimization
+
+# Write your package code here.
+
+end
---
+++ test/runtests.jl
@@ -0,0 +1,6 @@
+using VisualGeometryOptimization
+using Test
+
+@testset "VisualGeometryOptimization.jl" begin
+    # Write your tests here.
+end
