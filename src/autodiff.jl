0d1740ada26d68c51ddca6edcc9cf915a732518e
Oliver J. Woodford <o.j.woodford.98@cantab.net>
2025-05-16 21:21:09 +0100

    Remove autodiff normalize

--- src/autodiff.jl
+++ src/autodiff.jl
@@ -19,30 +19,3 @@ Base.:*(r::AbstractMatrix, u::RotMatLie{T}) where T = SMatrix{3, 3, T, 9}(T(r[1,
 Base.:*(u::RotMatLie{T}, r::AbstractMatrix) where T = SMatrix{3, 3, T, 9}(T(r[1,1], du(u.y)*r[3,1]-du(u.z)*r[2,1]), T(r[2,1], du(u.z)*r[1,1]-du(u.x)*r[3,1]), T(r[3,1], du(u.x)*r[2,1]-du(u.y)*r[1,1]),
                                                                           T(r[1,2], du(u.y)*r[3,2]-du(u.z)*r[2,2]), T(r[2,2], du(u.z)*r[1,2]-du(u.x)*r[3,2]), T(r[3,2], du(u.x)*r[2,2]-du(u.y)*r[1,2]),
                                                                           T(r[1,3], du(u.y)*r[3,3]-du(u.z)*r[2,3]), T(r[2,3], du(u.z)*r[1,3]-du(u.x)*r[3,3]), T(r[3,3], du(u.x)*r[2,3]-du(u.y)*r[1,3]))
-
-function mean0norm1!!(vec::T; minsqnorm=V(1e-10)) where T <: AbstractVector{FD} where FD <: ForwardDiff.Dual{V, P, N} where {V, P, N}
-    # Normalize a vector to have zero mean and unit norm
-    # Use an algorithm that only loads the data twice
-    x = zero(V)
-    xx = zero(V)
-    p = zeros(SVector{N, P})
-    px = zeros(SVector{N, P})
-    @simd for v in vec
-        x  += ForwardDiff.value(v)
-        xx += ForwardDiff.value(v) * ForwardDiff.value(v)
-        p += ForwardDiff.partials(v)
-        px += ForwardDiff.partials(v) .* ForwardDiff.value(v)
-    end
-    vecmean = x / length(vec)
-    vecnorm = @fastmath V(1) / sqrt(max(xx - vecmean * x, minsqnorm))
-    px = (p .* vecmean - px) .* (vecnorm ^ 3)
-    p = p ./ length(vec)
-    mean = FD(vecmean, ForwardDiff.Partials{N, P}((p...,)))
-    norm = FD(vecnorm, ForwardDiff.Partials{N, P}((px...,)))
-    if ismutabletype(V)
-        vec .= (vec .- mean) .* norm
-    else
-        vec = (vec .- mean) .* norm
-    end
-    return vec
-end
