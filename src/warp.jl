1b498cb2a09c8b6bedc9d641423ac149ca6af316
Oliver J. Woodford <o.j.woodford.98@cantab.net>
2025-06-23 13:49:50 +0100

    Fix warp generation

--- src/warp.jl
+++ src/warp.jl
@@ -7,49 +7,57 @@ end
 
 macro defineprojectivewarp(name, flags)
     ndims = 0
-    drp = drn = dsp = dsn = dap = dan = db = ""
     if flags & 1 == 1
-        dx = string("updatevec[start+", ndims,  "]")
+        d13 = :(updatevec[start+$ndims])
         ndims += 1
     else
-        dx = "0"
+        d13 = 0
     end
     if flags & 2 == 2
-        dy = string("updatevec[start+", ndims,  "]")
+        d23 = :(updatevec[start+$ndims])
         ndims += 1
     else
-        dy = "0"
+        d23 = 0
     end
     if flags & 4 == 4
-        drp = string("+updatevec[start+", ndims,  "]")
-        drn = string("+updatevec[start+", ndims,  "]")
+        d12 = :(-updatevec[start+$ndims])
+        d21 = :(updatevec[start+$ndims])
         ndims += 1
+    else
+        d12 = 0
+        d21 = 0
     end
     if flags & 8 == 8
-        dsp = string("+updatevec[start+", ndims,  "]")
-        dsn = string("-2*updatevec[start+", ndims,  "]")
+        d11 = :(1+updatevec[start+$ndims])
+        d22 = :(1+updatevec[start+$ndims])
+        d33 = :(1-2*updatevec[start+$ndims])
         ndims += 1
+    else
+        d11 = 1
+        d22 = 1
+        d33 = 1
     end
     if flags & 16 == 16
-        dap = string("+updatevec[start+", ndims,  "]")
-        dan = string("-2*updatevec[start+", ndims,  "]")
+        d11 = :($d11+updatevec[start+$ndims])
+        d22 = :($d22-updatevec[start+$ndims])
         ndims += 1
     end
     if flags & 32 == 32
-        db = string("+updatevec[start+", ndims,  "]")
+        d12 = :($d12+updatevec[start+$ndims])
+        d21 = :($d21+updatevec[start+$ndims])
         ndims += 1
     end
     if flags & 64 == 64
-        dc = string("updatevec[start+", ndims,  "]")
+        d31 = :(updatevec[start+$ndims])
         ndims += 1
     else
-        dc = "0"
+        d31 = 0
     end
     if flags & 128 == 128
-        dd = string("updatevec[start+", ndims,  "]")
+        d32 = :(updatevec[start+$ndims])
         ndims += 1
     else
-        dd = "0"
+        d32 = 0
     end
 
     return esc(quote
@@ -59,9 +67,9 @@ macro defineprojectivewarp(name, flags)
         $name() = $name(SMatrix{3, 3, Float64, 9}(1.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0, 1.0))
         NLLSsolver.nvars(::$name) = static($ndims)
         function NLLSsolver.update(var::$name, updatevec, start=1)
-            return $name(var.mat * SMatrix{3, 3, eltype(updatevec), 9}(1 $dsp $dap, 0 $drp $db,  $dx, 
-                                                                       0 $drn $db,  1 $dsp $dan, $dy, 
-                                                                       $dc,         $dd,         1 $dsn))
+            return $name(var.mat * SMatrix{3, 3, eltype(updatevec), 9}($d11, $d12, $d13, 
+                                                                       $d21, $d22, $d23, 
+                                                                       $d31, $d32, $d33))
         end
     end)
 end
--- test/warp.jl
+++ test/warp.jl
@@ -1,14 +1,16 @@
-using VisualGeometryOptimization, NLLSsolver, Static 
+using VisualGeometryOptimization, NLLSsolver, Static, StaticArrays 
 
 @testset "warp.jl" begin
     # Test projective warp definition
     warp = VisualGeometryOptimization.HomographyWarp()
     @test NLLSsolver.nvars(warp) == static(8)
 
-    # # Test update function
-    # updatevec = zeros(7)
-    # updated_warp = NLLSsolver.update(warp, updatevec)
-    # @test updated_warp.mat == warp.mat
+    # Test update function
+    updatevec = [0.4, 0.1, 0.2, 0.01, 0.02, 0.03, -0.01, 0.04, 0.05, 0.7]
+    updated_warp = NLLSsolver.update(warp, updatevec, 2)
+    @test updated_warp.mat == SMatrix{3, 3, Float64, 9}(1.05, -0.02, 0.1,
+                                                        0.0, 0.99, 0.2,
+                                                        0.04, 0.05, 0.96)
 
     # # Test transform function
     # coords = SMatrix{2, 3, Float64, 6}(1.0, 2.0, 1.0, 3.0, 4.0, 1.0)
