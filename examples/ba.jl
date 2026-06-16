3fe858389eda6143e7397ff1e7e2845ea22347ea
Oliver J. Woodford <o.j.woodford.98@cantab.net>
2026-06-16 19:30:52 +0100

    Add a bundle adjustment example

---
+++ examples/ba.jl
@@ -0,0 +1,13 @@
+using VisualGeometryOptimization, NLLSsolver
+
+problem = loadBALproblem("problem-16-22106")
+options = NLLSOptions(; reldcost=1.0e-6, maxiters=100)
+NLLSsolver.robustkernel(::BALResidual) = HuberKernel(3.)
+
+function myfun(problem, options)
+    result = optimize!(problem, options, nothing, printoutcallback)
+    display(result)
+    return nothing
+end
+
+myfun(problem, options)
