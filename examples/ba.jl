403c6147bcc89f72d14d1687fb9659b042e11d4b
Oliver J. Woodford <o.j.woodford.98@cantab.net>
2026-06-16 20:01:40 +0100

    Fix error

--- examples/ba.jl
+++ examples/ba.jl
@@ -2,7 +2,7 @@ using VisualGeometryOptimization, NLLSsolver
 
 problem = loadBALproblem("problem-16-22106")
 options = NLLSOptions(; reldcost=1.0e-6, maxiters=100)
-NLLSsolver.robustkernel(::BALResidual) = HuberKernel(3.)
+NLLSsolver.robustkernel(::VisualGeometryOptimization.BALResidual) = HuberKernel(3.)
 
 function myfun(problem, options)
     result = optimize!(problem, options, nothing, printoutcallback)
